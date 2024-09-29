ebaa70fdf65277d0efa1aa18125264f813bdfec0
jaximoto <62913915+jaximoto@users.noreply.github.com>
2024-09-28 22:19:39 -0700

    Working on simon says

--- Assets/Scripts/RoundTimer.cs
+++ Assets/Scripts/RoundTimer.cs
@@ -20,7 +20,7 @@ public class RoundTimer : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(StartTimer(roundLength));
-            Debug.Log("started Coroutine");
+            //Debug.Log("started Coroutine");
         }
         if (controllable) { 
             //Debug.Log("controllable");
@@ -34,7 +34,7 @@ public class RoundTimer : MonoBehaviour
         controllable = true;
         yield return new WaitForSeconds(roundDeltaTime);
         roundLength -= roundDeltaTime;
-        Debug.Log("roundLength = " + roundLength);
+        //Debug.Log("roundLength = " + roundLength);
 
         if (roundLength > 0) { StartCoroutine(StartTimer(roundLength)); }
         else
