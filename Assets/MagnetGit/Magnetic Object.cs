875150145e6cd86c5ba04e0f2c2768fb79a8738a
jaximoto <62913915+jaximoto@users.noreply.github.com>
2024-09-30 16:17:11 -0700

    Got magnets to work

---
+++ Assets/MagnetGit/Magnetic Object.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticObject : MonoBehaviour
+{
+    public enum Pole
+    {
+        North,
+        South
+    }
+
+    public float MagnetForce;
+    public Pole MagneticPole;
+    public Rigidbody RigidBody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        RigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnDrawGizmos()
+    {
+
+    }
+}
---
+++ Assets/MagnetGit/MagneticPoleEnum.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticPoleEnum : MonoBehaviour
+{
+    public enum Pole
+    {
+        North,
+        South
+    }
+
+    public float MagnetForce;
+    public Pole MagneticPole;
+    public Rigidbody RigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
