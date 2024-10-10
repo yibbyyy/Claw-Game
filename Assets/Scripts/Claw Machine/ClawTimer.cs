14d12c654b838607fbcd501706ee5484e358dffa
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2024-10-10 10:17:53 -0700

    claw timer setup

--- Assets/Scripts/Claw Machine/ClawTimer.cs
+++ Assets/Scripts/Claw Machine/ClawTimer.cs
@@ -6,15 +6,24 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ClawTimer : MonoBehaviour
 {
-    
+    public GameObject clawTimerDisplay;
+    Transform digit;
+
     public float clawTimerDuration;
     public float clawTimer;
+    private int clawTimerInt;
     public bool clawTimerRunning = false;
     
     public TMP_Text clawTimerUI;
 
     public static event Action ClawTimerEnded;
-    
+
+
+    private void Awake()
+    {
+        digit = clawTimerDisplay.GetComponentInChildren<Transform>();
+        Debug.Log("transform = " + digit.name);
+    }
     private void Start()
     {
         clawTimer = clawTimerDuration;
@@ -63,5 +72,27 @@ public class ClawTimer : MonoBehaviour
         clawTimer = clawTimerDuration;
         UpdateTimerUI();
     }
-    
+
+    private void IntToSprite()
+    {
+        clawTimerInt = Mathf.RoundToInt(clawTimer);
+
+
+        /*
+        for (int i = 0; i < places.Count; i++)
+        {
+            //If the string is shorter than the expected length, adds a 0 to the left
+            string tempString = timerCountString.PadLeft(places.Count, '0');
+            //Gets sprite renderer from the indexed child 
+            SpriteRenderer spriteRenderer = transform.GetChild(i).GetComponent<SpriteRenderer>();
+
+            //Takes passed string and indexes into it, returning the integer at that index
+            //Indexing into a string returns the character as unicode(ASCII)
+            //By subtracting the ASCII number for 0(48) from any int 0-9, the integer will be returned as an int
+            int dig = tempString[i] - '0';
+            Sprite newSprite = sprites[dig];
+            spriteRenderer.sprite = newSprite;
+        }
+        */
+    }
 }
