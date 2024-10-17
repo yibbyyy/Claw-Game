1aa44516652602d566b897888fbdf6b47eaaca8c
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2024-10-17 15:33:41 -0700

    game brunboller

--- Assets/Scripts/Game Controller.cs
+++ Assets/Scripts/Game Controller.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -25,12 +27,22 @@ public class GameController : MonoBehaviour
     {
         
     }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        Debug.Log("OnEnable called");
+    }
 
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+    }
     void Start()
     {
-        Setup += SetUpGame;
-        Setup?.Invoke();
-        GameTimer.timedOut += GameOver;
+        //Setup += SetUpGame;
+        //Setup?.Invoke();
+        //GameTimer.timedOut += GameOver;
     }
 
     // Update is called once per frame
