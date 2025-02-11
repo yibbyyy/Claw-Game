46a07314511ee4bf09142132c5f301e8965e9420
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2025-02-10 21:04:26 -0800

    music fix

--- Assets/Scripts/GameMusic.cs
+++ Assets/Scripts/GameMusic.cs
@@ -1,25 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GameMusic : MonoBehaviour
 {
-    public AudioClip track;
-    public AudioClip aTrack;
+    public AudioClip track1, track2;
     AudioSource source;
-
-
-    void Start()
+    float volume, startVolume;
+    public float deltaVolume;
+    private void Awake()
     {
-        StartButton.click += StartPlayback;
+        DontDestroyOnLoad(gameObject);
         source = GetComponent<AudioSource>();
-    }
+        source.clip = track1;
+        source.Play();
 
+        StartButton.click += StartPlayback;
+    }
+    
     void StartPlayback()
     {
-        source.clip = track;
-        source.Play();
+        volume = source.volume;
+        startVolume = volume;
+
+        StartCoroutine(PlayOut(volume));
+        
+
         StartButton.click -= StartPlayback;
     }
+
+
+    //smooth transition to main track
+    private IEnumerator PlayOut(float volume)
+    {
+
+        Debug.Log($"GameMusic PlayOut(volume {volume}) called");
+        while (volume > 0) 
+        {
+            volume -= deltaVolume;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        if (volume <= 0) 
+        {
+            source.clip = track2;
+            StartCoroutine(PlayIn(volume));
+            yield return null;
+        }
+        
+    }
+
+    private IEnumerator PlayIn(float volume)
+    {
+        Debug.Log($"GameMusic PlayIn(volume {volume}) called");
+        while (volume < startVolume) 
+        {
+            volume += deltaVolume;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        if (volume >= startVolume)
+        {
+            
+            volume = startVolume;
+            yield return null;
+        }
+        
+    }
+
+
 }
